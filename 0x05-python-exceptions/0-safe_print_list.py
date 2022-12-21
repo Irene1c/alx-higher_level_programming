893bd103e5705bf1980d6f8e7faef8ea1052d66b
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2022-12-21 05:06:08 -0800

    fix exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
         n = 0
         for i in range(x):
             print(my_list[i], end="")
-            n+=1
+            n += 1
         print()
-    except:
+    except IndexError:
         print("")
 
     return n

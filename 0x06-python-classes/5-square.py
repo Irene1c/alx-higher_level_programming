ca8353b9a287469eb6dff9689908c88ba44ca7b6
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-01-09 09:44:22 -0800

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,7 +5,7 @@
 class Square:
     """A class Square that defines a square"""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):

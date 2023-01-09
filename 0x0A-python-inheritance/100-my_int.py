dbaa52f726a86d29ef5bdde023d832cdf78a4963
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-01-09 14:03:36 -0800

    comparison magic methods

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,17 +4,20 @@
 
 class MyInt(int):
     """ MyInt has == and != operators inverted """
+
     def __init__(self, other):
         super().__init__()
 
     def __eq__(self, other):
+
         """ evaluates if two instances are equal
             In the current class it evaluates to not equal to
         """
-        return  super().__ne__(other)
+        return super().__ne__(other)
 
     def __ne__(self, other):
+
         """ evaluates if two instances are not equal
             In the current class it evaluates to equal to
         """
-        return  super().__eq__(other)
+        return super().__eq__(other)

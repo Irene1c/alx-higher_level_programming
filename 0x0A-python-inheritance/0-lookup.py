8ca28a700d1c1e1d8f6dfec2f83fc673a6401b54
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-01-09 10:30:29 -0800

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def lookup(obj):
+
     """ 
     returns the list of available attributes and
     methods of an object.
     """
+
+
+def lookup(obj):
+    """ returns a list """
     return dir(obj)

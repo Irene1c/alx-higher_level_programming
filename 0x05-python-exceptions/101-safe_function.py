2bdd65e11daa884915ed9031d2740f7622c5b6b2
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-03-28 09:52:29 -0700

    fix Exception to stderr

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from sys import stderr
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as e:
-        print("Exception:", e)
+        stderr.write(f"Exception: {e}\n")
         return None

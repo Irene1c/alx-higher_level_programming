8f106c9cbcefbd542f87e38e8e93ee4bc5559ee7
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-11 12:38:14 -0700

    An instance method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defining a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ a class that BaseGeometry """
+
+    def area(self):
+        """ raising an Exception """
+        raise Exception("area() is not implemented")

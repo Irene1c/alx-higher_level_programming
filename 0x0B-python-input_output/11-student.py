162a8648291d4109363d5036d9303af34bd86740
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-12 11:38:13 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A class Student that defines a student"""
+
+
+class Student:
+    """A class Student that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance
+            If attrs is a list of strings, only attribute names contained
+            in this list must be retrieved.
+        """
+
+        if type(attrs) is not list:
+            return self.__dict__
+        else:
+            list_names = {}
+            for name in attrs:
+                str_list = getattr(self, name, None)
+                if str_list is not None:
+                    list_names[name] = str_list
+            return list_names
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+
+        for val in json:
+            self.__dict__[val] = json[val]

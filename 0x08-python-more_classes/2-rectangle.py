e4b9f77873169877d5b9b3f22cde4af192bfde2a
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-16 11:57:09 -0700

    Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -41,11 +41,11 @@ class Rectangle:
 
     def area(self):
         """ returns the rectangle area """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """ returns the rectangle perimeter """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return ((self.width + self.height) * 2)
+            return ((self.__width + self.__height) * 2)

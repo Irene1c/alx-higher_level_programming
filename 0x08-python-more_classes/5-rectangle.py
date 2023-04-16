76f39aaa5f4c00b47d91aaaf05e9777fc56696df
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-16 12:39:16 -0700

    Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -41,14 +41,14 @@ class Rectangle:
 
     def area(self):
         """ returns the rectangle area """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """ returns the rectangle perimeter """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return ((self.width + self.height) * 2)
+            return ((self.__width + self.__height) * 2)
 
     def __str__(self):
         """print the rectangle with the character #"""

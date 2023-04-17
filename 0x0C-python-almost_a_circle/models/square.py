55a6fe573c129942b1a545ce44121e09d23f5244
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-17 12:05:56 -0700

    A class Square that inherits from Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,20 +8,16 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.__width = size
-        self.__height = size
-        self.__x = x
-        self.__y = y
 
     def __str__(self):
         """return a printable string representation of the instance"""
-        return f"[Square] ({self.id}) {self.__x}/"\
-            f"{self.__y} - {self.__width}"
+        return f"[Square] ({self.id}) {self.x}/"\
+            f"{self.y} - {self.width}"
 
     @property
     def size(self):
         """ getting the size"""
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -31,49 +27,7 @@ class Square(Rectangle):
         elif value <= 0:
             raise ValueError("width must be > 0")
         else:
-            self.__width = value
-
-    @property
-    def x(self):
-        """ getting the valuee of x"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """setting the value of x"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
-
-    @property
-    def y(self):
-        """ getting the value of y"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """setting the value of y"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
-
-    def display(self):
-        """prints in stdout the Rectangle instance with the character #"""
-
-        for b in range(self.__y):
-            print("")
-        for i in range(self.__height):
-            for a in range(self.__x):
-                print(" ", end="")
-            for j in range(self.__width):
-                print("#", end="")
-            print("")
+            self.width = value
 
     def update(self, *args, **kwargs):
         """assigning arguments to attributes"""

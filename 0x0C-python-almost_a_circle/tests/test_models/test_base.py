b1f5b03047075d26c98c4343559a4b40808bfb00
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-19 13:57:38 -0700

    test for JSON string to dictionary

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,7 +2,6 @@
 """unittest for the Base class"""
 import unittest
 from models.base import Base
-import json
 from models.rectangle import Rectangle
 from models.square import Square
 
@@ -36,6 +35,24 @@ class TestBase(unittest.TestCase):
         d2 = '[{"id": 49, "size": 2, "x": 4, "y": 7}]'
         self.assertEqual(d2, Base.to_json_string([s_dict]))
         self.assertIsInstance(Base.to_json_string([s_dict]), str)
+
+        self.assertEqual("[]", Base.to_json_string([]))
+        self.assertIsInstance(Base.to_json_string([]), str)
+
+    def test_from_json_string(self):
+        """test for JSON string to dictionary """
+
+        d3 = '{"id": 59, "width": 6, "height": 4, "x": 5, "y": 9}'
+        a_list = Rectangle.from_json_string(d3)
+        li1 = {'id': 59, 'width': 6, 'height': 4, 'x': 5, 'y': 9}
+        self.assertEqual(li1, a_list)
+        self.assertIsInstance(a_list, dict)
+
+        d4 = '{"id": 49, "size": 2, "x": 4, "y": 7}'
+        b_list = Square.from_json_string(d4)
+        li2 = {'id': 49, 'size': 2, 'x': 4, 'y': 7}
+        self.assertEqual(li2, b_list)
+        self.assertIsInstance(b_list, dict)
 
 
 if __name__ == "__main__":

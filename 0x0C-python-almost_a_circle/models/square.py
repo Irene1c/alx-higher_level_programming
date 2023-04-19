5e6faa6219fec75706195d20a5ca3bc92a77d5a0
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-04-19 05:18:56 -0700

    update square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,21 +7,26 @@ class Square(Rectangle):
     """A class Square that inherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """ class constructor """
+
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """return a printable string representation of the instance"""
+
         return f"[Square] ({self.id}) {self.x}/"\
             f"{self.y} - {self.width}"
 
     @property
     def size(self):
         """ getting the size"""
+
         return self.width
 
     @size.setter
     def size(self, value):
         """setting the value of size"""
+
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -31,6 +36,7 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """assigning arguments to attributes"""
+
         n = ('id', 'size', 'x', 'y')
         if args:
             for i in range(len(args)):

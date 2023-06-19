7d7e7a5107bca9366d51c45a794914a47628cebc
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-19 10:07:14 -0700

    update First state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  script that prints the first State object from the database hbtn_0e_6_usa"""
+"""script that prints the first State object from the database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     session = Session()
 
     States = session.query(State).order_by(State.id).first()
-    if States:
-        print(f"{States.id}: {States.name}")
+    if States is None:
+        print('Nothing')
     else:
-        print()
+        print(f"{States.id}: {States.name}")

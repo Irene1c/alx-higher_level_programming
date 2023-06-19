5ef4ad53b84c3b3dea51bb2d1fd613f363184d1b
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-19 05:20:19 -0700

    update model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,14 +3,7 @@
     instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
-import MySQLdb
-
-
-db = MySQLdb.connect(host="localhost", port=3306,
-                     user=argv[1], passwd=argv[2], db=argv[3])
 
 
 Base = declarative_base()
@@ -23,9 +16,3 @@ class State(Base):
 
     id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/:3306{}'.format
-                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)

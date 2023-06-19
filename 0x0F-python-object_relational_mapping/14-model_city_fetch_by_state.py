061f729289ccfcffa6ddd79ad693e48bbd4b4dea
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-06-19 13:56:57 -0700

    print Cities in states

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that prints all City objects from the database hbtn_0e_14_usa"""
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv, exit
+
+
+if __name__ == "__main__":
+    if len(argv) < 4:
+        exit()
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}\
+            @localhost:3306/{database}', pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    q = session.query(City, State).join(State).order_by(City.id.asc()).all()
+    for city, state in q:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+session.close()

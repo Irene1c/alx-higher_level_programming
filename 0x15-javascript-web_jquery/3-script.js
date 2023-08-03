bdffb25b8848b5a1d3d47dc9dde0276028d0bd6e
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-08-03 14:07:08 -0700

    Add red class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+//  script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
+
+$('#red_header').on('click', function (event) {
+  $('header').addClass('red');
+});

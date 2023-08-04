b63023c80dea0047842c5b0406fe9b734325bdc4
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-08-04 06:11:21 -0700

    script that fetches and displays the value of hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,13 @@
+/*
+ script that fetches and displays the value of hello from that fetch in the HTML tag DIV#hello
+ Your script must work when it is imported from the <head> tag
+*/
+
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data) {
+    const charHello = data.hello;
+    $('#hello').text(charHello);
+  }).fail(function (error) {
+    console.error(error);
+  });
+});
